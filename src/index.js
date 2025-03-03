d7bbe35425f0f8f1ffb54c5bc8436773347a592d
Doug Sellers <DouglasSSellers@gmail.com>
2025-03-03 17:42:55 -0500

    App clean up, deleted unused components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import AppV1 from './AppV1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
